06f7a32749249f3309a2905dbe75f687f40e6e2e
rayct <rayct@icloud.com>
2023-03-25 02:13:58 +0000

    Added Logo

--- weather.py
+++ weather.py
@@ -12,15 +12,14 @@ root.title("Weather App")
 root.geometry("1000x600+300+200")
 root.resizable(False,False)
 
+
 # Search Box
 # Search_image=PhotoImage(file="")
 def search():
     # code to perform search goes here
     search_term = entry.get()
     print(f"Searching for: {search_term}")
-
 # root = tk.Tk()
-
 entry = tk.Entry(root, justify="center", width=30)
 entry.pack()
 entry.place(x=60, y=40)
@@ -28,6 +27,14 @@ entry.place(x=60, y=40)
 button = tk.Button(root, justify="center", text="Search", command=search)
 button.pack()
 button.place(x=350, y=39)
+
+# Logo
+Logo_image=PhotoImage(file="logo.png")
+logo=Label(image=Logo_image)
+logo.place(x=150, y=100)
+
+
+# Bottom Box
 
 
 
